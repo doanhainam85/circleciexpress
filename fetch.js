748d4adbb5499e29ad8a87daa742e215c805b8ab
Doan Hai Nam <Nam.Hai.Doan@vn.centralretail.com>
2021-11-07 17:32:57 +0700

    change fetch

--- fetch.js
+++ fetch.js
@@ -8,6 +8,7 @@ async function fetchAsync() {
   // only proceed once promise is resolved
   let data = await response.json();
   // only proceed once second promise is resolved
+//Test
   return data;
 }
 // trigger async function
